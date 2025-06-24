00d6f6a4b4632ff551bb0b06a50548cbc061c95c
RUTH-1979 <ruthfotio.l@gmail.com>
2025-06-24 16:07:44 +0100

    modification de la formule total

---
+++ Note.js
@@ -0,0 +1,14 @@
+const notes=[
+    [9,8,14],
+    [15,11,13],
+    [10,17]
+];
+let total=0;
+let nbrNotes=0;
+for(let matiere of notes) {
+    for(let note of matiere);
+}
+total = total +note;
+nbrNotes++;{
+    console.log(total/nbrNotes)
+}
