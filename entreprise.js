ad173a9bc56980455783d3467a4f4e9ccdf87c7e
RUTH-1979 <ruthfotio.l@gmail.com>
2025-06-26 21:16:36 +0100

    ajout de secteur

--- entreprise.js
+++ entreprise.js
@@ -2,8 +2,12 @@ var entreprise = {
     nom :"softsec",
     adresse :"Akwa nord",
     télephone :"695638195",
-}
+    email :"softsec.@gmail.com",
+    secteur :"Douala 3eme",
+};
 console.log("Informationsdel'entreprise:");
 console.log("Nom:[entreprise.nom]");
 console.log("Adresse:[entreprise.adresse]");
 console.log("Telephone:[entreprise.telephone]");
+console.log("Email:[entreprise.email]");
+console.log("Secteur:[entreprise.secteur]");
