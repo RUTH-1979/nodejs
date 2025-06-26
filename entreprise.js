e2f5aaf4b484ff4f88798610d7b4cf6b7a6ce985
RUTH-1979 <ruthfotio.l@gmail.com>
2025-06-26 21:02:42 +0100

    ajout de l'email

---
+++ entreprise.js
@@ -0,0 +1,9 @@
+var entreprise = {
+    nom :"softsec",
+    adresse :"Akwa nord",
+    télephone :"695638195",
+}
+console.log("Informationsdel'entreprise:");
+console.log("Nom:[entreprise.nom]");
+console.log("Adresse:[entreprise.adresse]");
+console.log("Telephone:[entreprise.telephone]");
